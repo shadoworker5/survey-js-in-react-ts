6e3390d9838d2a7b653dafdf39377962ee0200e5
Kassoum Traore <kassoum.traore@africasys.com>
2025-01-08 00:14:21 +0000

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,19 @@ import Navbar from './Navbar.tsx';
 import SurveyrBuilder from './survey/builder.tsx';
 import CreateForm from './survey/surveyJson.tsx';
 import ShowData from './pages/show_data.tsx';
+import Home from './pages/home.tsx';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-        <Navbar />
-        <Routes>
-            <Route path="/survey_builder" element={<SurveyrBuilder />} />
-            <Route path="/form_example" element={<CreateForm />} />
-            <Route path="/show_data" element={<ShowData />} />
-        </Routes>
-    </Router>
-);
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/survey_builder" element={<SurveyrBuilder />} />
+				<Route path="/form_example" element={<CreateForm />} />
+				<Route path="/show_data" element={<ShowData />} />
+			</Routes>
+		</Router>
+	);
 }
-
 export default App;
---
+++ src/pages/home.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="container mt-4">
+                <div className="card">
+                    <div className="card-body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ipsa consequuntur eligendi placeat architecto quasi commodi harum praesentium. Perspiciatis, repellat rerum pariatur sequi delectus odit quibusdam debitis culpa nisi ipsum!
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Home;
